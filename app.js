8c1ce4f77de9c1aff964564f280252139c5f6d81
saiteja-aie <saiteja.aiengineer@gmail.com>
2023-11-02 01:00:35 +0530

    3rd commit changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey! This is my first Continuous Integration/Continuous Deployment Pipeline.'));
+app.get('/', (req, res) => res.send('Hey! This is my first Continuous Integration(CI)/Continuous Deployment(CD) Pipeline.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
